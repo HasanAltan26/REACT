cd074e2675c5f92654291484bb505569d5ccdd8d
HasanAltan <58146726+HasanAltan26@users.noreply.github.com>
2022-04-22 18:31:47 +0200

    8.DAY

---
+++ Learning/8.DAY/Project-Tech Shop-1/script.js
@@ -0,0 +1,16 @@
+
+var message = document.getElementById("message");
+var but = document.getElementById("button")
+
+
+
+
+function shop(){
+
+    message.innerHTML=  Number(message.innerHTML)+ 1
+
+}
+
+
+
+
