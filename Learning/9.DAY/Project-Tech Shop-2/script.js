ac5af5db0328912d36a860f723b33d58cb6e9001
HasanAltan <58146726+HasanAltan26@users.noreply.github.com>
2022-04-23 21:06:55 +0200

    9.DAY

---
+++ Learning/9.DAY/Project-Tech Shop-2/script.js
@@ -0,0 +1,12 @@
+
+var but1 = document.getElementById("button1")
+var but2 = document.getElementById("button2")
+var but3 = document.getElementById("button3")
+var but4 = document.getElementById("button4")
+var but5 = document.getElementById("button5")
+var but6 = document.getElementById("button6")
+
+
+
+
+
